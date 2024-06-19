b5628677d4290ccdea8c0b19f60f6e6c38318633
Jinu Vijayan <jinuvijayan01@gmail.com>
2024-06-19 15:30:27 +0530

    Add: added conneting to data base

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 10000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI)
+.then(()=>{
+    console.log("Connected to data base");
+})
+.catch(err=>{
+    console.log("Error occured while connecting to data base");
+});
 
 app.listen(PORT,()=>{
     console.log("Server is running on port", PORT)
